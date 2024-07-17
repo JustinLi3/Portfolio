9530bdb983e2f4818d5c6567f81d021a0f68ca0a
JustinLi3 <Triamethyest@gmail.com>
2024-07-17 15:18:27 -0400

    Saving

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-$("#intro-description").
-$("")
+$(document).ready(function(){ 
+    $("#intro").css("cursor","pointer")
+    $("#intro").click(function() {  
+        if($("#intro-description").attr("hidden")==null){  
+            $("#intro-facecard").removeClass("reveal"); 
+            setTimeout(() => {
+                $("#intro-description").attr("hidden",true);  
+            }, 100);
+        } 
+        else{            
+            $("#intro-facecard").addClass("reveal"); 
+            setTimeout(() => {
+                $("#intro-description").removeAttr("hidden");   
+            }, 100);
+
+        }
+       
+    });
+});
