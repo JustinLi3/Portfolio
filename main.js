a5d22ac61ae1d913850bcefa9d9aa590f2e0b1aa
JustinLi3 <Triamethyest@gmail.com>
2024-07-25 20:51:50 -0400

    Saving

--- main.js
+++ main.js
@@ -17,5 +17,25 @@ $(document).ready(function(){
 
         }
        
+    }); 
+    const text = document.querySelector(".sec-text");
+    const textLoad = () => {
+        setTimeout(() => {
+            text.textContent = "Developer";
+        }, 0);
+        setTimeout(() => {
+            text.textContent = "Designer";
+        }, 4000);
+        setTimeout(() => {
+            text.textContent = "Freelancer";
+        }, 8000); //1s = 1000 milliseconds
+    }
+    textLoad();
+    setInterval(textLoad, 12000);  
+
+    $(window).scroll(function(){
+        $("#welcome-text").css("opacity", 1 - $(window).scrollTop()/500);
+        $("#description1").css("opacity", 1 - $(window).scrollTop()/500);
     });
+    
 });
